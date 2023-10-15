92a45faf157495fabe17ef5b7e54ec4821ffa494
gointoorbit <kania.anna@onet.eu>
2023-10-15 10:29:09 +0200

    removed hw10 holdover

--- src/cat-api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// axios.defaults.headers.common['x-api-key'] =
-//   'live_geId1CRDozUUaO3wvuE4mlk7XQeKCStlZxRKAFXUjHAtQc3rujqItjEkh0gIVSrD';
-
-
-
-// export { fetchBreeds, fetchCatByBreed };
